db4998450fbb799114534bee8f6f0c3a16b34b92
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-02-20 10:18:53 -0500

    fleshed out position calculations

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ window.onmousemove = (e) => {
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
     maxDelta = window.innerWidth / 2;
 
-  const percentage = (mouseDelta / maxDelta) * -100;
+  const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
-  track.style.transform = `translate(${percentage}%, -50%)`;
+  track.dataset.percentage = nextPercentage;
+
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+  for (const image of track.getElementsByClassName('image')) {
+    image.style.objectPosition = `${nextPercentage + 100}% 50%`;
+  }
+};
+
+window.onmouseup = () => {
+  track.dataset.mouseDownAt = 0;
+  track.dataset.prevPercentage = track.dataset.percentage;
 };
